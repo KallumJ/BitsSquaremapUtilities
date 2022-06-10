347b25fd622d9ef584c3b0860f03db6b83a405a7
Kallum <kallummj071001@gmail.com>
2022-06-10 20:20:09 +0100

    Added save on server stop

--- src/main/java/team/bits/squaremap/utils/BitsSquaremapUtils.java
+++ src/main/java/team/bits/squaremap/utils/BitsSquaremapUtils.java
@@ -1,12 +1,21 @@
 package team.bits.squaremap.utils;
 
 import net.fabricmc.api.ModInitializer;
+import org.jetbrains.annotations.NotNull;
+import team.bits.nibbles.event.base.EventManager;
+import team.bits.nibbles.event.server.ServerStoppingEvent;
+import team.bits.squaremap.utils.map.MapManager;
 
 
-public class BitsSquaremapUtils implements ModInitializer {
+public class BitsSquaremapUtils implements ModInitializer, ServerStoppingEvent.Listener {
 
 	@Override
 	public void onInitialize() {
+		EventManager.INSTANCE.registerEvents(this);
+	}
 
+	@Override
+	public void onServerStopping(@NotNull ServerStoppingEvent event) {
+		MapManager.save();
 	}
 }

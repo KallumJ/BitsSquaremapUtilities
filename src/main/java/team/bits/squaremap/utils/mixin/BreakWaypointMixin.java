fe6e34c5f805c22d70ac3aa7be48a018844632df
Kallum <kallummj071001@gmail.com>
2022-06-15 11:44:32 +0100

    Updated mixin to remove marker if banner is broken by non player means

--- src/main/java/team/bits/squaremap/utils/mixin/BreakWaypointMixin.java
+++ src/main/java/team/bits/squaremap/utils/mixin/BreakWaypointMixin.java
@@ -2,30 +2,32 @@ package team.bits.squaremap.utils.mixin;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.entity.BlockEntity;
-import net.minecraft.server.network.ServerPlayerInteractionManager;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 import team.bits.squaremap.utils.map.MapManager;
 
-@Mixin(ServerPlayerInteractionManager.class)
+@Mixin(World.class)
 public class BreakWaypointMixin {
-
     @Inject(
-            method = "tryBreakBlock",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/block/Block;onBroken(Lnet/minecraft/world/WorldAccess;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;)V"
-            ),
-            locals = LocalCapture.CAPTURE_FAILHARD
+            method = "setBlockState(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;II)Z",
+            at = @At("HEAD")
     )
-    public void onBreak(BlockPos pos, CallbackInfoReturnable<Boolean> cir, BlockState blockState, BlockEntity blockEntity, Block block, boolean bl) {
-        if (MapManager.isBlockTypeOfMarker(block)) {
-            MapManager.removeMarkerAtPosIfPresent(pos);
+    public void test(BlockPos pos, BlockState state, int flags, int maxUpdateDepth, CallbackInfoReturnable<Boolean> cir) {
+        World world = (World) (Object) this;
+        Block block = world.getBlockState(pos).getBlock();
+        // If block having state updated could be a waypoint
+        if (MapManager.isBlockTypeOfWaypoint(block)) {
+
+            // If state is being updated with air (being broken)
+            if (state.isAir()) {
+
+                // Remove the waypoint, if it is present
+                MapManager.removeMarkerAtPosIfPresent(pos);
+            }
         }
     }
 }
